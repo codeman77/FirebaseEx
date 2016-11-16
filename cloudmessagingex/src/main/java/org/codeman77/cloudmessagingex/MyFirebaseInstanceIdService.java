73aeb3af34de5a14e2dc84052bb00dfc2c913caf
codeman77 <rkswl0320>
2016-11-16 19:32:45 +0900

    Firebase Cloud Messaging Ex 추가

---
+++ cloudmessagingex/src/main/java/org/codeman77/cloudmessagingex/MyFirebaseInstanceIdService.java
@@ -0,0 +1,20 @@
+package org.codeman77.cloudmessagingex;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+
+        // token이 갱신 되면 발생함
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Firebase", "Refreshed token: " + refreshedToken);
+
+        // 토큰을 DB에 저장하거나 따로 관리해주는 동작이 필요함
+
+    }
+}
---
+++ cloudmessagingex/src/main/java/org/codeman77/cloudmessagingex/MyFirebaseMessagingService.java
@@ -0,0 +1,18 @@
+package org.codeman77.cloudmessagingex;
+
+
+        import android.util.Log;
+
+        import com.google.firebase.messaging.FirebaseMessagingService;
+        import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // 포그라운드에서만 작동함
+        Log.d("Firebase", "From: " + remoteMessage.getFrom());
+        Log.d("Firebase", "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+
+}
